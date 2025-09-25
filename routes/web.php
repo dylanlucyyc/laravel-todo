c4ed7debd8ea05022b6e56c5038d7e8618613278
Dylan Luc <dylanlucyyc@gmail.com>
2025-09-24 19:05:18 -0600

    Routing

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,29 @@
 
 use Illuminate\Support\Facades\Route;
 
+// GET -> Display
+// POST -> Create
+// PUT -> Modify 
+// DELETE -> Delete
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
+});
+
+Route::get('/hello', function(){
+    return 'Hello';
+})->name('hello');
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello '. $name . '!';
+});
+
+// Redirect URL 
+Route::get('/hallo', function() {
+    return redirect()->route('hello');
+});
+
+// Fallback route 
+Route::fallback(function () {
+    return '404 Page';
 });
